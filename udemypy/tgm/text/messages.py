a34da2ae9f58be7a19e2d84c67ae036088c20444
Dylan Tintenfich <tintenfichdylan@gmail.com>
2021-09-02 18:10:26 -0300

    Modify donate button text

--- udemypy/tgm/text/messages.py
+++ udemypy/tgm/text/messages.py
@@ -1,12 +1,12 @@
 from udemypy.tgm.text import emojis
 
 
-get_course_button_text = f'Get course {emojis.PERSON_RUNNING}'
+get_course_button_text = f"Get course {emojis.PERSON_RUNNING}"
 
-share_button_text = f'Share channel {emojis.SPEAKING_HEAD}'
+share_button_text = f"Share channel {emojis.SPEAKING_HEAD}"
 
-donate_button_text = f'Donate us {emojis.HEART}'
+donate_button_text = f"Donate me {emojis.HEART}"
 
 
 def message_title(course_link, course_title) -> str:
-    return f'[{emojis.TICKET}]({course_link}) _*Free Udemy Course:*_\n\n{course_title}'
+    return f"[{emojis.TICKET}]({course_link}) _*Free Udemy Course:*_\n\n{course_title}"
