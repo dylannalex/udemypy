f5f8b35936a0c8c9a79f99a93b3019b265e6e676
Dylan Tintenfich <tintenfichdylan@gmail.com>
2021-09-20 19:54:15 -0300

    Replace single quotes with double quotes

--- udemypy/tgm/tgm_data.py
+++ udemypy/tgm/tgm_data.py
@@ -1,7 +1,7 @@
 from os import getenv
 
 
-TOKEN = getenv('TOKEN')
-CHANNEL_ID = '@freecourses000'
-DONATE_ME_LINK = getenv('DONATE_ME_LINK')
-CHANNEL_LINK = getenv('CHANNEL_LINK')
+TOKEN = getenv("TOKEN")
+CHANNEL_ID = "@freecourses000"
+DONATE_ME_LINK = getenv("DONATE_ME_LINK")
+CHANNEL_LINK = getenv("CHANNEL_LINK")
