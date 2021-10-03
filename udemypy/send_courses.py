445d28ca8f188aa2a9a6b8efeb89cc897329b904
Dylan Tintenfich <tintenfichdylan@gmail.com>
2021-10-03 01:06:01 -0300

    Catch exceptions while sending courses

--- udemypy/send_courses.py
+++ udemypy/send_courses.py
@@ -3,5 +3,12 @@ from udemypy.twitter import twitter_bot
 
 
 def send_courses(courses):
-    tgm_bot.send_courses(courses)
-    twitter_bot.send_courses(courses)
+    try:
+        tgm_bot.send_courses(courses)
+    except Exception as exception:
+        print(f"\n\nCould not complete Telegram sending process\nERROR:{exception}")
+
+    try:
+        twitter_bot.send_courses(courses)
+    except Exception as exception:
+        print(f"\n\nCould not complete Twitter sending process\nERROR:{exception}")
