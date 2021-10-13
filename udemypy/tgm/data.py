d5e83ef3a21ad2e015223591f6b04eda195ee385
Dylan Tintenfich <tintenfichdylan@gmail.com>
2021-10-13 16:34:59 -0300

    CHANNEL_ID is now an environment variable
    This allows to configure the telegram channel id without changing
    the source code.

--- udemypy/tgm/data.py
+++ udemypy/tgm/data.py
@@ -2,6 +2,6 @@ from os import getenv
 
 
 TOKEN = getenv("TOKEN")
-CHANNEL_ID = "@freecourses000"
+CHANNEL_ID = getenv("CHANNEL_ID")
 DONATE_ME_LINK = getenv("DONATE_ME_LINK")
 CHANNEL_LINK = getenv("CHANNEL_LINK")
