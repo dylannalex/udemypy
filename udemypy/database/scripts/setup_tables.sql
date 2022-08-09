dbe59c73b9b8b3a2c91da72b6514173b3be98b52
Dylan Tintenfich <tintenfichdylan@gmail.com>
2022-08-09 20:43:32 -0300

    Create `setup_tables.sql`

---
+++ udemypy/database/scripts/setup_tables.sql
@@ -0,0 +1,24 @@
+/* Setup social media */
+INSERT INTO social_media(
+    id,
+    name,
+    udemypy_username,
+    udemypy_profile_link)
+VALUES(
+    0,
+    "Twitter",
+    "@UdemyPy",
+    "https://twitter.com/UdemyPy"
+);
+
+INSERT INTO social_media(
+    id,
+    name,
+    udemypy_username,
+    udemypy_profile_link)
+VALUES(
+    1,
+    "Telegram",
+    "@freecourses000",
+    "https://t.me/freecourses000"
+);
