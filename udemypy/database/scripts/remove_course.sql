8ec53e38638364fa8e8f9b9204dfe721961a43af
Dylan Tintenfich <tintenfichdylan@gmail.com>
2022-08-10 23:24:25 -0300

    Fix foreign key constraint error

--- udemypy/database/scripts/remove_course.sql
+++ udemypy/database/scripts/remove_course.sql
@@ -1,5 +1,2 @@
-DELETE c, csm FROM 
-    course AS c,
-    course_social_media AS csm
-WHERE c.id = id_value
-    AND c.id = csm.course_id;
+DELETE FROM course_social_media WHERE course_id=id_value;
+DELETE FROM course WHERE id=id_value;
