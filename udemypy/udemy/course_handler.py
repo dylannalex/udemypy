aa8516e945611cf8f40f0702385c235c777fb8e6
Dylan Tintenfich <tintenfichdylan05@gmail.com>
2023-12-23 18:41:10 -0300

    Add courses stats concurrently

--- udemypy/udemy/course_handler.py
+++ udemypy/udemy/course_handler.py
@@ -1,6 +1,7 @@
 from udemypy.udemy import scraper
 from udemypy import course
 from udemypy.udemy import settings
+from concurrent.futures import ThreadPoolExecutor
 
 
 def _delete_duplicated_courses(courses: list[dict]) -> list[dict]:
@@ -14,7 +15,7 @@ def _scrape_courses(pages: int) -> list[course.Course]:
     # Scrape courses
     courses_scrapers = (
         scraper.DiscudemyScraper(pages),
-        # scraper.UdemyFreebiesScraper(pages),
+        scraper.UdemyFreebiesScraper(pages),
         # scraper.TutorialBarScraper(pages),
     )
     scraped_courses = []
@@ -44,50 +45,57 @@ def _delete_shared_courses_dict(
     return [c for c in courses if c.title not in titles]
 
 
-def add_courses_stats(courses: list[course.Course]) -> list[course.Course]:
-    courses_with_stats = []
-    for course_ in courses:
+def add_course_stats(
+    course_: course.Course,
+    stats_scraper: scraper.StatsScraper = None,
+) -> course.Course:
+    """Adds stats to a single course."""
+    if stats_scraper is None:
         stats_scraper = scraper.StatsScraper(
             settings.CHROMEDRIVER_PATH,
             settings.GOOGLE_CHROME_BIN,
             settings.PAGE_LOAD_TIME,
         )
-        # stats_scrapper is inside the for loops so
-        # the webdriver is refreshed on each course
-        # link and prevent Udemy to hide course stats
-        try:
-            stats = stats_scraper.get_stats(course_.link_with_coupon)
-        except Exception as exception:
-            print(f"[ERROR] Error while adding courses stats: {exception}")
-
-        # Create Course instance with its stats added
-        courses_with_stats.append(
-            course.Course(
-                stats["id"],
-                course_.title,
-                course_.link,
-                course_.coupon_code,
-                course_.date_found,
-                discount=stats["discount"],
-                discount_time_left=stats["discount_time_left"],
-                students=stats["students"],
-                rating=stats["rating"],
-                language=stats["language"],
-                badge=stats["badge"],
-            )
+    try:
+        stats = stats_scraper.get_stats(course_.link_with_coupon)
+        return course.Course(
+            id=stats["id"],
+            title=course_.title,
+            link=course_.link,
+            coupon_code=course_.coupon_code,
+            date_found=course_.date_found,
+            discount=stats["discount"],
+            discount_time_left=stats["discount_time_left"],
+            students=stats["students"],
+            rating=stats["rating"],
+            language=stats["language"],
+            badge=stats["badge"],
         )
-    return courses_with_stats
+    except Exception as exception:
+        print(f"[ERROR] Error while adding courses stats: {exception}")
+
+
+def add_courses_stats(
+    courses: list[course.Course],
+    max_workers: int = 5,
+) -> list[course.Course]:
+    """Adds stats to a list of courses concurrently."""
+    with ThreadPoolExecutor(max_workers) as executor:
+        courses_with_stats = list(executor.map(add_course_stats, courses))
+    return [c for c in courses_with_stats if c is not None]
 
 
 def new_courses(shared_courses: list[course.Course]) -> list[dict]:
     """
     Find free Udemy courses, deletes already shared
-    courses and returns them with their stats added.
+    courses and returns them without their stats added.
+
+    Note: the new courses found have id=None, as id is
+    scraped by StatsScraper.
     """
     scraped_courses = _scrape_courses(settings.PAGES_TO_SCRAPE)
     new_courses = _delete_shared_courses_dict(scraped_courses, shared_courses)
-    new_courses_with_stats = add_courses_stats(new_courses)
-    return new_courses_with_stats
+    return new_courses
 
 
 def delete_non_free_courses(
