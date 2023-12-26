9f9a584c1273556ede139c1c20e4c0ac0ed6cc4c
Dylan Tintenfich <tintenfichdylan05@gmail.com>
2023-12-26 10:25:35 -0300

    Remove tests

--- tests/_constants.py
+++
@@ -1,9 +0,0 @@
-from udemypy.udemy import course
-
-EXAMPLE_COURSE = course.Course(
-    id=1,
-    title="CourseTitle",
-    link="CourseLink.com",
-    coupon_code="ABCDEF",
-    date_found="0000-00-00 00:00:00",
-)
