e49f8326a264e197fef0f193d3f851e14a42bf28
Dylan Tintenfich <tintenfichdylan05@gmail.com>
2024-04-23 16:11:53 -0300

    Add bot handlers to scheduler

--- udemypy/scheduler.py
+++ udemypy/scheduler.py
@@ -6,22 +6,65 @@ from udemypy.udemy import settings as ud_settings
 from udemypy import settings
 from udemypy.sender import TelegramBot, WhatsAppBot
 from datetime import datetime, timedelta
+from abc import abstractmethod
+from abc import ABC
 from time import sleep
 from os import system
 
 
-def schedule():
+class BotHandler(ABC):
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def send_courses(self, verbose: bool):
+        pass
+
+
+class TelegramHandler(BotHandler):
+    def __init__(self):
+        self.bot = TelegramBot(
+            settings.TOKEN,
+            settings.CHANNEL_ID,
+            settings.CHANNEL_LINK,
+            settings.GITHUB_LINK,
+            settings.WHATSAPP_LINK,
+        )
+
+    def send_courses(self, db):
+        print("[Telegram Bot]")
+        self.bot.connect()  # Reconnects every iteration
+        send_courses.send_courses(
+            db,
+            self.bot,
+            db_settings.TELEGRAM_NAME,
+            db_settings.TELEGRAM_ID,
+        )
+
+
+class WhatsAppHandler(BotHandler):
+    def __init__(self):
+        self.whatsapp_bot = WhatsAppBot(ud_settings.CHROMEDRIVER_PATH)
+        self.whatsapp_bot.connect()  # Only first connection
+
+    def send_courses(self, db):
+        # WhatsApp Bot
+        print("[WhatsApp Bot]")
+        send_courses.send_courses(
+            db,
+            self.bot,
+            db_settings.WHATSAPP_NAME,
+            db_settings.WHATSAPP_ID,
+        )
+
+
+def schedule_bots(
+    bot_handlers: list[BotHandler],
+    waiting_seconds: int,
+    iterations: int,
+):
     db = database.connect()
-    whatsapp_bot = WhatsAppBot(ud_settings.CHROMEDRIVER_PATH)
-    whatsapp_bot.connect()
-    telegram_bot = TelegramBot(
-        settings.TOKEN,
-        settings.CHANNEL_ID,
-        settings.CHANNEL_LINK,
-        settings.GITHUB_LINK,
-        settings.WHATSAPP_LINK,
-    )
-    waiting_time = 60 * 30  # 30 min
+    waiting_seconds = 60 * 30  # 30 min
     iterations = 10
 
     for iteration in range(iterations):
@@ -31,35 +74,24 @@ def schedule():
         print("[Finding Courses]")
         find_courses.find_courses(db, verbose=True)
 
-        # Telegram Bot
-        print("[Telegram Bot]")
-        telegram_bot.connect()  # Reconnects every iteration
-        send_courses.send_courses(
-            db,
-            telegram_bot,
-            db_settings.TELEGRAM_NAME,
-            db_settings.TELEGRAM_ID,
-        )
-
-        # WhatsApp Bot
-        print("[WhatsApp Bot]")
-        send_courses.send_courses(
-            db,
-            whatsapp_bot,
-            db_settings.WHATSAPP_NAME,
-            db_settings.WHATSAPP_ID,
-        )
+        # Send courses
+        for bot_handler in bot_handlers:
+            bot_handler.send_courses(db)
 
         # Calculate the time of the next iteration
-        next_iteration_time = datetime.now() + timedelta(seconds=waiting_time)
+        next_iteration_time = datetime.now() + timedelta(seconds=waiting_seconds)
         next_iteration_time = next_iteration_time.strftime("%H:%M:%S")
         print(f"\n[-] Next iteration at: {next_iteration_time}")
 
         # Sleep
-        sleep(waiting_time)
+        sleep(waiting_seconds)
 
     db.close()
 
 
 if __name__ == "__main__":
-    schedule()
+    schedule_bots(
+        bot_handlers=[TelegramHandler()],
+        waiting_seconds=60 * 30,
+        iterations=20,
+    )
