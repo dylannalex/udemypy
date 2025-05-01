11774c45fa29d0bdb421040e81c36e2d34b65182
Dylan Tintenfich <tintenfichdylan05@gmail.com>
2025-05-01 12:56:26 -0300

    Catch bot import error

--- udemypy/sender/__init__.py
+++ udemypy/sender/__init__.py
@@ -1,4 +1,22 @@
 from udemypy.sender.bot import SenderBot
-from udemypy.sender.whatsapp_bot import WhatsAppBot
-from udemypy.sender.tgm_bot import TelegramBot
-from udemypy.sender.twitter_bot import TwitterBot
+try:
+    from udemypy.sender.whatsapp_bot import WhatsAppBot
+except ImportError:
+    WhatsAppBot = None
+    print(
+        "[Warning] WhatsApp Bot not available."
+    )
+try:
+    from udemypy.sender.tgm_bot import TelegramBot
+except ImportError:
+    TelegramBot = None
+    print(
+        "[Warning] Telegram Bot not available."
+    )
+try:
+    from udemypy.sender.twitter_bot import TwitterBot
+except ImportError:
+    TwitterBot = None
+    print(
+        "[Warning] Twitter Bot not available."
+    )
